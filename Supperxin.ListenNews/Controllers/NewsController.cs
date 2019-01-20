3b2dcc0f1ef07135c510a4e07ae1628e6c39adf6
Jinxin Chen <xin3222634@163.com>
2019-01-20 21:59:55 +0800

    add view news

---
+++ Supperxin.ListenNews/Controllers/NewsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Supperxin.ListenNews.Data;
+using Supperxin.ListenNews.Models;
+
+namespace Supperxin.ListenNews.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: News
+        public async Task<IActionResult> Index(DateTime? date)
+        {
+            var queryDate = date ?? DateTime.Now;
+            var dateStart = queryDate.Date;
+            var dateEnd = queryDate.Date.AddDays(1);
+
+            var viewModel = new NewsViewModel();
+            viewModel.News = await _context.Item.Where(i => i.Created >= dateStart && i.Created < dateEnd).Take(120).ToArrayAsync();
+            viewModel.Dates = await (from item in _context.Item
+                                     group item by item.Created.Date into g
+                                     orderby g.Key descending
+                                     select new DateCount
+                                     {
+                                         DateString = g.Key.ToString("yyyy-MM-dd"),
+                                         Count = g.Count()
+                                     }).Take(10).ToArrayAsync();
+            //.GroupBy(i => i.Created.Date).OrderByDescending(i => i.Key).Take(10).SelectMany().ToArrayAsync();
+
+            return View(viewModel);
+        }
+
+        // GET: News/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Item
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // GET: News/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: News/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Url,Title,Content,Category,Source,Created,CrawledTime,AudioStatus,AudioErrorMessage")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        // GET: News/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Item.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+
+        // POST: News/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Url,Title,Content,Category,Source,Created,CrawledTime,AudioStatus,AudioErrorMessage")] Item item)
+        {
+            if (id != item.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(item);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ItemExists(item.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        // GET: News/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Item
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        // POST: News/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var item = await _context.Item.FindAsync(id);
+            _context.Item.Remove(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ItemExists(int id)
+        {
+            return _context.Item.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Supperxin.ListenNews/Models/NewsViewModel.cs
@@ -0,0 +1,14 @@
+namespace Supperxin.ListenNews.Models
+{
+    public class NewsViewModel
+    {
+        public DateCount[] Dates { get; set; }
+        public Item[] News { get; set; }
+    }
+
+    public class DateCount
+    {
+        public string DateString { get; set; }
+        public int Count { get; set; }
+    }
+}
