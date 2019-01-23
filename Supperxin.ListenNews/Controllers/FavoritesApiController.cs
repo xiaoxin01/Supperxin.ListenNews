1fb59ab24fdc1552a906a19a0dd8af8e345a4c06
Jinxin Chen <xin3222634@163.com>
2019-01-23 22:17:46 +0800

    add page to show favorites news

---
+++ Supperxin.ListenNews/Controllers/FavoritesApiController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Supperxin.ListenNews.Data;
+using Supperxin.ListenNews.Models;
+
+namespace Supperxin.ListenNews.Controllers
+{
+    [Authorize]
+    [Route("api/Favorites")]
+    [ApiController]
+    public class FavoritesApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public FavoritesApiController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: api/Favorites
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Favorite>>> GetFavorite()
+        {
+            return await _context.Favorite.Take(10).ToListAsync();
+        }
+
+        // GET: api/Favorites/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Favorite>> GetFavorite(int id)
+        {
+            var favorite = await _context.Favorite.FindAsync(id);
+
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+
+            return favorite;
+        }
+
+        // POST: api/Favorites
+        [HttpPost]
+        public async Task<ActionResult<Favorite>> PostFavorite(Favorite favorite)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var existFavorite = await _context.Favorite.FirstOrDefaultAsync(f => f.ItemId == favorite.ItemId && !f.Deleted && f.UserId == user.Id);
+            if (null != existFavorite)
+            {
+                existFavorite.Deleted = true;
+                existFavorite.Modified = DateTime.Now;
+                await _context.SaveChangesAsync();
+                return Ok(new { status = 1, message = "☆" });
+            }
+
+            favorite.UserId = user.Id;
+            _context.Favorite.Add(favorite);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { status = 1, message = "★" });
+        }
+
+        // DELETE: api/Favorites/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Favorite>> DeleteFavorite(int id)
+        {
+            var favorite = await _context.Favorite.FindAsync(id);
+            if (favorite == null)
+            {
+                return NotFound();
+            }
+
+            _context.Favorite.Remove(favorite);
+            await _context.SaveChangesAsync();
+
+            return favorite;
+        }
+
+        private bool FavoriteExists(int id)
+        {
+            return _context.Favorite.Any(e => e.Id == id);
+        }
+    }
+}
--- Supperxin.ListenNews/Controllers/FavoritesController.cs
+++ Supperxin.ListenNews/Controllers/FavoritesController.cs
@@ -1,92 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Supperxin.ListenNews.Data;
-using Supperxin.ListenNews.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Supperxin.ListenNews.Controllers
 {
     [Authorize]
-    [Route("api/[controller]")]
-    [ApiController]
-    public class FavoritesController : ControllerBase
+    public class FavoritesController : Controller
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<IdentityUser> _userManager;
-
+        private readonly int PageSize = 500;
         public FavoritesController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
             _userManager = userManager;
         }
-
-        // GET: api/Favorites
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Favorite>>> GetFavorite()
-        {
-            return await _context.Favorite.Take(10).ToListAsync();
-        }
-
-        // GET: api/Favorites/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Favorite>> GetFavorite(int id)
-        {
-            var favorite = await _context.Favorite.FindAsync(id);
-
-            if (favorite == null)
-            {
-                return NotFound();
-            }
-
-            return favorite;
-        }
-
-        // POST: api/Favorites
-        [HttpPost]
-        public async Task<ActionResult<Favorite>> PostFavorite(Favorite favorite)
+        public async Task<IActionResult> Index()
         {
             var user = await _userManager.GetUserAsync(User);
-            var existFavorite = await _context.Favorite.FirstOrDefaultAsync(f => f.ItemId == favorite.ItemId && !f.Deleted && f.UserId == user.Id);
-            if (null != existFavorite)
-            {
-                existFavorite.Deleted = true;
-                existFavorite.Modified = DateTime.Now;
-                await _context.SaveChangesAsync();
-                return Ok(new { status = 1, message = "☆" });
-            }
+            // query items with user's favorite status
+            var query = from item in _context.Item
+                        join favorite in (from f in _context.Favorite where f.UserId == user.Id && !f.Deleted select f)
+                        on item.Id equals favorite.ItemId
+                        select new { Item = item, Favorite = favorite };
 
-            favorite.UserId = user.Id;
-            _context.Favorite.Add(favorite);
-            await _context.SaveChangesAsync();
+            var items = (await query.ToListAsync()).Select(x => { x.Item.FavoriteId = x.Favorite.Id; return x.Item; }).Take(PageSize).ToArray();
 
-            return Ok(new { status = 1, message = "★" });
-        }
-
-        // DELETE: api/Favorites/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Favorite>> DeleteFavorite(int id)
-        {
-            var favorite = await _context.Favorite.FindAsync(id);
-            if (favorite == null)
-            {
-                return NotFound();
-            }
-
-            _context.Favorite.Remove(favorite);
-            await _context.SaveChangesAsync();
-
-            return favorite;
-        }
-
-        private bool FavoriteExists(int id)
-        {
-            return _context.Favorite.Any(e => e.Id == id);
+            return View(items);
         }
     }
 }
