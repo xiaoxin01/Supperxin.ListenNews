3120b937ea8ea7d623425b46868c7708e3e864df
Jinxin Chen <xin3222634@163.com>
2019-01-27 11:55:16 +0800

    add back to top

--- Supperxin.ListenNews/wwwroot/js/site.js
+++ Supperxin.ListenNews/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(window).scroll(function () {
+    if ($(this).scrollTop()) {
+        $('#toTop').fadeIn();
+    } else {
+        $('#toTop').fadeOut();
+    }
+});
+
+$("#toTop").click(function () {
+    //1 second of animation time
+    //html works for FFX but not Chrome
+    //body works for Chrome but not FFX
+    //This strange selector seems to work universally
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+});
